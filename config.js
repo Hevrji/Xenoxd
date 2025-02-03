658b75f31b9b2438e503ec13354286eb49b5471e
Hevrji <116875997+Hevrji@users.noreply.github.com>
2025-02-03 12:19:04 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const global = {
-    owner: [7011435951], // ganti jadi id mu
-    botToken: "7723126109:AAG35MigEOg18SYWx5oUOV7DjXSS2OM6lok", //isi make token bot mu
-}
-
-module.exports = global;
